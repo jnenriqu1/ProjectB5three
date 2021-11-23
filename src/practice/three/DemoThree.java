3386f69a635e2fb1b9a11586f049386a37f9862d
jnenr <jnenr@DESKTOP-TUIGB2O.myfiosgateway.com>
2021-11-22 22:10:25 -0500

    commit and push practice...

---
+++ src/practice/three/DemoThree.java
@@ -0,0 +1,8 @@
+package practice.three;
+
+public class DemoThree {
+
+	public static void main(String[] args) {
+		System.out.println("this is the last practice to commit and push to GitHub");
+	}
+}
